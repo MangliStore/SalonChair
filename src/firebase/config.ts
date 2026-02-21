7d79ec6d60e1e559dae2adca3b55a5210e7d3bdd
CITI Mobiles <citimobilesknr@gmail.com>
2026-02-21 17:50:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5370622301-6620e",
+  "appId": "1:24658204750:web:380f06a42c621035d98b1f",
+  "apiKey": "AIzaSyDirtjPCrjTwgCt8xmgmKvgj6TynS8V-AI",
+  "authDomain": "studio-5370622301-6620e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "24658204750"
+};
