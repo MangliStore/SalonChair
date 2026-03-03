f4544290536a29e7b721fc6cc50a1ecae5a9be99
CITI Mobiles <citimobilesknr@gmail.com>
2026-03-03 09:45:18 +0000

    in logo change the chair icon, the chair should face to the letter r in

--- src/components/chair-icon.tsx
+++ src/components/chair-icon.tsx
@@ -12,11 +12,15 @@ export function ChairIcon({ className }: { className?: string }) {
       strokeLinejoin="round"
       className={className}
     >
-      <path d="M7 18V5c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v13" />
-      <path d="M5 18h14" />
-      <path d="M19 13H5" />
-      <path d="M15 18v3" />
-      <path d="M9 18v3" />
+      {/* Backrest on the right side */}
+      <path d="M18 4v12" />
+      {/* Seat extending left towards the text */}
+      <path d="M18 12H8a2 2 0 0 0-2 2v1" />
+      {/* Armrest */}
+      <path d="M18 9H11" />
+      {/* Hydraulic Base silhouette */}
+      <path d="M13 17v4" />
+      <path d="M9 21h8" />
     </svg>
   );
 }
